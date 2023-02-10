5de8baaa8c2b4d0e90b5c2ae970948094e92d1df
LuisKevL <20213tn118@utez.edu.mx>
2023-02-10 13:29:54 -0600

    creacion de boton

--- src/components/Boton.js
+++ src/components/Boton.js
@@ -1,1 +1,13 @@
-rnfs
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function Boton() {
+  return (
+    <View>
+      <Text>Boton</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
+
